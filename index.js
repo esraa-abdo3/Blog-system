62f73ff2e684b5077ef145a3ab783fbd2e809d97
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-04-07 15:31:48 +0200

    test production

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const passport = require("passport");
-
+const mongoconnect = require("./config/dbconnect.js");
 const app = express();
 
 // Middleware
@@ -14,15 +14,8 @@ app.use(express.json());
 app.use(passport.initialize());
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    retryWrites: true,
-    w: "majority",
-  })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.log("❌ MongoDB connection error:", err));
+
+mongoconnect()
 
 // Routes
 const authRoutes = require("../../routes/Authroutes.js");
@@ -35,6 +28,7 @@ const postRoutes = require("../../routes/postsroutes.js");
 app.use("/api/v1/posts", postRoutes);
 
 const groupRoutes = require("../../routes/GroupRoutes.js");
+
 app.use("/api/v1/groups", groupRoutes);
 
 // Global 404
@@ -50,4 +44,4 @@ app.use((err, req, res, next) => {
 });
 
 // بدل app.listen نعمل export للـ handler
-module.exports.handler = serverless(app);
+module.exports = app;
