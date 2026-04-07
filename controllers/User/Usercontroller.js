f95694acbbd0177f391f90cab2f56828d5738e69
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-04-07 16:21:45 +0200

    v1 production

--- controllers/User/Usercontroller.js
+++ controllers/User/Usercontroller.js
@@ -10,7 +10,7 @@ const getAllUsers = Asncwrapper(async (req, res) => {
   const limit = parseInt(req.query.limit) || 2;
   const page = parseInt(req.query.page) || 1;
   const skip = (limit * (page - 1)); 
- const users = await User.find({ _v: 0 }).limit(limit).skip(skip);
+    const users = await User.find({},{ _v: 0 }).limit(limit).skip(skip);
     res.status(200).json({ status: "Success", data: users, msg: "All users retrieved successfully" });
 })
 //2- get user by id
