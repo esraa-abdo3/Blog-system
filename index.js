5b7e9ddde10e30fe7160a07ca2f15ea318f48744
esraa-abdo3 <esraaabdalnasserzz@gmail.com>
2026-05-10 22:28:56 +0300

    fix

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const mongoconnect = require("./config/dbconnect.js");
 const app = express();
 
 // Middleware
-app.use(cors());
+
+
+
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// مهم جداً — handle الـ OPTIONS manually
+app.options('*', cors());
 app.use(express.json());
 app.use(passport.initialize());
 
